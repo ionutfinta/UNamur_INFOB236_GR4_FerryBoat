e7e8b082de06289cf4975bcfe489d1b44c2fd005
Finta <ionut.finta@student.unamur.be>
2020-05-09 17:59:51 +0200

    LeaveLift.java corrigé

--- Processing/generated_java_ferryBoat_sequential/src/ferryBoat_sequential/LeaveLift.java
+++ Processing/generated_java_ferryBoat_sequential/src/ferryBoat_sequential/LeaveLift.java
@@ -19,7 +19,14 @@ public class LeaveLift{
  		assignable \nothing;
 		ensures \result <==> (machine.get_lift_level().equals(new Integer(1)) && machine.VEHICLE_TYPES.has(vehicle_type) && (machine.get_bs_m()).compareTo(new Integer(0)) > 0 && (machine.vehicle_slot.apply(vehicle_type)).compareTo(machine.get_bs_m()) <= 0 && machine.get_reservations().domain().has(vehicle_id) && new Integer(new JMLObjectSet {Integer fInteger v | (exists Integer e; (machine.floors.has(f) && v.equals(vehicle_type)); e.equals(new Pair<Integer,Integer>(f,v)))}).has(machine.get_reservations().apply(vehicle_id)) && machine.get_lift_access().equals(true) && machine.get_lift_in().equals(true)); */
 	public /*@ pure */ boolean guard_LeaveLift( Integer vehicle_id, Integer vehicle_type) {
-		return (machine.get_lift_level().equals(new Integer(1)) && machine.VEHICLE_TYPES.has(vehicle_type) && (machine.get_bs_m()).compareTo(new Integer(0)) > 0 && (machine.vehicle_slot.apply(vehicle_type)).compareTo(machine.get_bs_m()) <= 0 && machine.get_reservations().domain().has(vehicle_id) && new Integer(new JMLObjectSet {Integer fInteger v | (exists Integer e; (machine.floors.has(f) && v.equals(vehicle_type)); e.equals(new Pair<Integer,Integer>(f,v)))}).has(machine.get_reservations().apply(vehicle_id)) && machine.get_lift_access().equals(true) && machine.get_lift_in().equals(true));
+		return (machine.get_lift_level().equals(new Integer(1)) // grd3_1
+				&& machine.VEHICLE_TYPES.has(vehicle_type) //grd1_2
+				&& (machine.get_bs_m()).compareTo(new Integer(0)) > 0 //grd1_2
+				&& (machine.vehicle_slot.apply(vehicle_type)).compareTo(machine.get_bs_m()) <= 0 //grd2_2
+				&& machine.get_reservations().domain().has(vehicle_id) //grd3_2
+				//grd4_2 était déjà erronée en EventB: On exprime la même chose que grd3_2
+				&& machine.get_lift_access().equals(true) 
+				&& machine.get_lift_in().equals(true));
 	}
 
 	/*@ public normal_behavior
@@ -39,7 +46,12 @@ public class LeaveLift{
 
 			machine.set_bs_m(new Integer(bs_m_tmp - machine.vehicle_slot.apply(vehicle_type)));
 			machine.set_in_lift_ids(in_lift_ids_tmp.difference(new BSet<Integer>(vehicle_id)));
-			machine.set_lift_vehicles(lift_vehicles_tmp.difference(new no_type(new JMLObjectSet {Pair<Integer,Integer> x | (\exists no_type e; (BRelation.cross(new Enumerated(1,machine.queues),new Enumerated(1,machine.lift_depth)).has(null) && lift_vehicles_tmp.apply(null).equals(vehicle_id)); e.equals(new Pair<Integer,ERROR>(null,vehicle_id)))})));
+			//Même souci que dans Board.java:
+			//machine.set_lift_vehicles(lift_vehicles_tmp.difference(new Pair<Integer,Integer> x | (\exists no_type e; (BRelation.cross(new Enumerated(1,machine.queues),new Enumerated(1,machine.lift_depth)).has(null) && lift_vehicles_tmp.apply(null).equals(vehicle_id)); e.equals(new Pair<Integer,Integer>(null,vehicle_id)))})));
+			for(Pair<Integer, Integer> k: lift_vehicles_tmp.inverseElementImage(vehicle_id)) {
+				lift_vehicles_tmp.remove(k, vehicle_id);
+			}
+			machine.set_lift_vehicles(lift_vehicles_tmp);
 			machine.set_boarded_ids(boarded_ids_tmp.difference(new BSet<Integer>(vehicle_id)));
 
 			System.out.println("LeaveLift executed vehicle_id: " + vehicle_id + " vehicle_type: " + vehicle_type + " ");
