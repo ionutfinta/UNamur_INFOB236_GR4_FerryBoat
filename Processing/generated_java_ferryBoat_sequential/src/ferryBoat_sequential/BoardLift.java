fd68b7cbc97569402a8ff24cc4287e7402c0288e
Finta <ionut.finta@student.unamur.be>
2020-05-09 17:31:20 +0200

    BoardLift.java corrigé

--- Processing/generated_java_ferryBoat_sequential/src/ferryBoat_sequential/BoardLift.java
+++ Processing/generated_java_ferryBoat_sequential/src/ferryBoat_sequential/BoardLift.java
@@ -13,13 +13,38 @@ public class BoardLift{
 	public BoardLift(fifthRef m) {
 		this.machine = m;
 	}
+	
+	// Donne le slot le plus profond disponible sur la plateforme du lift selon la file et les slots requis, 0 dans le cas échéant.
+	private int getMinSlot(int queue, int needed_slots) {
+		for(int i = 1; i <= machine.lift_depth - needed_slots; i++) {
+			if(machine.get_lift_vehicles().domain().has(new Pair<Integer, Integer>(queue, i)))
+				continue;
+			return i;
+		}
+		return 0;
+	}
 
 	/*@ public normal_behavior
 		requires true;
  		assignable \nothing;
 		ensures \result <==> ((new Integer(machine.vehicle_slot.apply(vehicle_type) + machine.get_bs_m())).compareTo(machine.max_bs_m) <= 0 && machine.get_lift_level().equals(new Integer(1)) && machine.get_auth_on_ids().has(vehicle_id) && machine.VEHICLE_TYPES.has(vehicle_type) && new Integer(new JMLObjectSet {Integer fInteger v | (exists Integer e; (machine.floors.has(f) && v.equals(vehicle_type)); e.equals(new Pair<Integer,Integer>(f,v)))}).has(machine.get_reservations().apply(vehicle_id)) && new Enumerated(new Integer(1),machine.queues).has(position_q) && ((machine.get_id_is_left().apply(vehicle_id).equals(true)) ==> (position_q.equals(new Integer(1)))) && ((machine.get_id_is_left().apply(vehicle_id).equals(false)) ==> (position_q.equals(new Integer(2)))) && new Enumerated(new Integer(1),machine.lift_depth).has(position_s) && position_s.equals((new BSet<Integer>(new Integer(machine.lift_depth + new Integer(1))).union(new Integer(new JMLObjectSet {Integer x | (\exists Integer e; (new Enumerated(new Integer(1),new Integer(new Integer(4) - machine.vehicle_slot.apply(vehicle_type))).has(x) && (x).compareTo((new BSet<Integer>(new Integer(0)).union(new Integer(new JMLObjectSet {Integer y | (\exists Integer e; (new Enumerated(new Integer(1),machine.lift_depth).has(y) && machine.get_lift_vehicles().domain().has(new Pair<Integer,Integer>(position_q,y))); e.equals(y))}))).max()) > 0); e.equals(x))}))).min()) && machine.get_lift_access().equals(true) && machine.get_lift_in().equals(true)); */
 	public /*@ pure */ boolean guard_BoardLift( Integer vehicle_id, Integer vehicle_type, Integer position_q, Integer position_s) {
-		return ((new Integer(machine.vehicle_slot.apply(vehicle_type) + machine.get_bs_m())).compareTo(machine.max_bs_m) <= 0 && machine.get_lift_level().equals(new Integer(1)) && machine.get_auth_on_ids().has(vehicle_id) && machine.VEHICLE_TYPES.has(vehicle_type) && new Integer(new JMLObjectSet {Integer fInteger v | (exists Integer e; (machine.floors.has(f) && v.equals(vehicle_type)); e.equals(new Pair<Integer,Integer>(f,v)))}).has(machine.get_reservations().apply(vehicle_id)) && new Enumerated(new Integer(1),machine.queues).has(position_q) && BOOL.implication(machine.get_id_is_left().apply(vehicle_id).equals(true),position_q.equals(new Integer(1))) && BOOL.implication(machine.get_id_is_left().apply(vehicle_id).equals(false),position_q.equals(new Integer(2))) && new Enumerated(new Integer(1),machine.lift_depth).has(position_s) && true && machine.get_lift_access().equals(true) && machine.get_lift_in().equals(true));
+		Integer max_bs_m = machine.max_bs_m;
+		BRelation<Integer, Integer> vehicle_slot = machine.vehicle_slot;
+		return (
+				(new Integer(vehicle_slot.apply(vehicle_type) + machine.get_bs_m())).compareTo(max_bs_m) <= 0 
+				&& machine.get_lift_level().equals(new Integer(1))
+				&& machine.get_auth_on_ids().has(vehicle_id) 
+				&& machine.VEHICLE_TYPES.has(vehicle_type) 
+				&& machine.get_reservations().domain().has(vehicle_id) // La garde 2_2 a été mal conçue en Event-B 
+				&& new Enumerated(new Integer(1),machine.queues).has(position_q)
+				&& BOOL.implication(machine.get_id_is_left().apply(vehicle_id).equals(true),position_q.equals(new Integer(1))) 
+				&& BOOL.implication(machine.get_id_is_left().apply(vehicle_id).equals(false),position_q.equals(new Integer(2))) 
+				&& new Enumerated(new Integer(1),machine.lift_depth).has(position_s)
+				&& position_s.equals(this.getMinSlot(position_q, machine.vehicle_slot.apply(vehicle_type)))
+				//La guarde 4_3 n'a même pas été générée: && true 
+				&& machine.get_lift_access().equals(true) 
+				&& machine.get_lift_in().equals(true));
 	}
 
 	/*@ public normal_behavior
@@ -42,7 +67,12 @@ public class BoardLift{
 			machine.set_auth_on_ids(auth_on_ids_tmp.difference(new BSet<Integer>(vehicle_id)));
 			machine.set_in_lift_ids((in_lift_ids_tmp.union(new BSet<Integer>(vehicle_id))));
 			machine.set_boarded_ids((new BSet<Integer>(vehicle_id).union(boarded_ids_tmp)));
-			machine.set_lift_vehicles((lift_vehicles_tmp.union(new BRelation<Pair<Integer,Integer>,Integer>(new JMLObjectSet {Pair<Integer,Integer> x | (\exists BRelation<Pair<Integer,Integer>,Integer> e; (BRelation.cross(new BSet<Integer>(position_q),new Enumerated(position_s,new Integer(new Integer(position_s + machine.vehicle_slot.apply(vehicle_type)) - 1))).has(null)); e.equals(new Pair<Integer,ERROR>(null,vehicle_id)))}))));
+			
+			// La génération par défaut pose problème:
+			//machine.set_lift_vehicles((lift_vehicles_tmp.union(new BRelation<Pair<Integer,Integer>,Integer>(new JMLObjectSet {Pair<Integer,Integer> x | (\exists BRelation<Pair<Integer,Integer>,Integer> e; (BRelation.cross(new BSet<Integer>(position_q),new Enumerated(position_s,new Integer(new Integer(position_s + machine.vehicle_slot.apply(vehicle_type)) - 1))).has(null)); e.equals(new Pair<Integer,ERROR>(null,vehicle_id)))}))));
+			// On la redéfinit comme suit:
+			lift_vehicles_tmp.insert(new Pair<Integer, Integer>(position_q, position_s), vehicle_id);
+			machine.set_lift_vehicles(lift_vehicles_tmp);
 
 			System.out.println("BoardLift executed vehicle_id: " + vehicle_id + " vehicle_type: " + vehicle_type + " position_q: " + position_q + " position_s: " + position_s + " ");
 		}
